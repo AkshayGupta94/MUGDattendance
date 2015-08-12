0c910d1eef480d8b6afe319188c0d5ce0aa169be
Akshay Kumar Gupta <akshaygupta1994@outlook.com>
2015-08-12 23:53:36 +0530

    attendance page done

--- AttendanceMugd/AttendanceMugd.WindowsPhone/Attendance.xaml.cs
+++ AttendanceMugd/AttendanceMugd.WindowsPhone/Attendance.xaml.cs
@@ -41,23 +41,37 @@ namespace AttendanceMugd
         }
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-
             myProgressBar.Visibility = Windows.UI.Xaml.Visibility.Visible;
             myProgressBar.IsIndeterminate = true;
             items = await Table
-                     .Where(Student => Student.Roll_no == Box.Text)
+                     .Where(Student => Student.Roll_no == Email.Text)
                       .ToCollectionAsync();
             Student a = new Student();
             a = items[0];
-            a.Attendance++;
-            a.LastAttended = DateTime.Today;
-            await Table.UpdateAsync(a);
-            myProgressBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            string mess = "Attndance of {0} has been updates succesfully";
-            string.Format(mess, a.FullName);
-            myProgressBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            MessageDialog msgbox = new MessageDialog(mess);
-            await msgbox.ShowAsync();
+            if (a.dateLastUpdated == int.Parse(DateTime.Now.Day.ToString()))
+            {
+                string mess = "Attndance of " + a.FullName + " has been already updated today";
+                myProgressBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                MessageDialog msgbox = new MessageDialog(mess);
+                await msgbox.ShowAsync();
+            }
+            else
+            {
+                a.Attendance++;
+                a.Consecutive++;
+                a.LastAttended = DateTime.Today;
+                a.dateLastUpdated = int.Parse(DateTime.Now.Day.ToString());
+                await Table.UpdateAsync(a);
+                myProgressBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                string mess2 = "Attndance of " + a.FullName + " has been updated succesfully";
+                myProgressBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                MessageDialog msgbox2 = new MessageDialog(mess2);
+                await msgbox2.ShowAsync();
+            }
+        }
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
