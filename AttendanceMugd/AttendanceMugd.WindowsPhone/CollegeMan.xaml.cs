2fa9557bf637f3ff4c0a27e05b00bf5c2d19fe04
Akshay Kumar Gupta <akshaygupta1994@outlook.com>
2015-08-13 00:01:36 +0530

    College page added!(Manage)

--- AttendanceMugd/AttendanceMugd.WindowsPhone/CollegeMan.xaml.cs
+++ AttendanceMugd/AttendanceMugd.WindowsPhone/CollegeMan.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,11 +24,31 @@ namespace AttendanceMugd
     /// </summary>
     public sealed partial class CollegeMan : Page
     {
+        private IMobileServiceTable<Colleges> Table2 = App.MobileService.GetTable<Colleges>();
+        private MobileServiceCollection<Colleges, Colleges> items2;
         public CollegeMan()
         {
             this.InitializeComponent();
         }
+        private async void submit_Click(object sender, RoutedEventArgs e)
+        {
+            Colleges item = new Colleges
+            {
+                collegeName = Namen.Text,
+                university = Email.Text
+            };
+            myProgressBar.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            myProgressBar.IsIndeterminate = true;
+            await App.MobileService.GetTable<Colleges>().InsertAsync(item);
+            myProgressBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            MessageDialog msgbox = new MessageDialog("College has been added succesfully");
+            await msgbox.ShowAsync();
+        }
 
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
         /// <summary>
         /// Invoked when this page is about to be displayed in a Frame.
         /// </summary>
