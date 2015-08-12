a53b15eff84178a602df576e7d09e4523a500282
Akshay Kumar Gupta <akshaygupta1994@outlook.com>
2015-08-13 00:03:20 +0530

    Idea Page added!

--- AttendanceMugd/AttendanceMugd.WindowsPhone/ideaPage.xaml.cs
+++ AttendanceMugd/AttendanceMugd.WindowsPhone/ideaPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,7 +27,51 @@ namespace AttendanceMugd
         {
             this.InitializeComponent();
         }
+        private async void submit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog m = new MessageDialog("");
 
+            if (title.Text.Length == 0)
+            {
+                m.Title = "enter title";
+                await m.ShowAsync();
+            }
+            else if (option_1.Text.Length == 0)
+            {
+                m.Title = "enter description";
+                await m.ShowAsync();
+            }
+            else if (option_2.Text.Length == 0)
+            {
+                m.Title = "enter issued by";
+                await m.ShowAsync();
+            }
+            else if (option_3.Text.Length == 0)
+            {
+                m.Title = "enter venue";
+                await m.ShowAsync();
+            }
+            else
+            {
+                idea temp = new idea();
+                temp.Title = title.Text;
+                temp.option1 = option_1.Text;
+                temp.option2 = option_2.Text;
+                temp.option3 = option_3.Text;
+                // entered into azure
+                myProgressBar.Visibility = Visibility.Visible;
+                myProgressBar.IsIndeterminate = true;
+                await App.MobileService.GetTable<idea>().InsertAsync(temp);
+                myProgressBar.Visibility = Visibility.Collapsed;
+                Windows.UI.Popups.MessageDialog messageBox = new MessageDialog("Idea has been added successfully");
+                await messageBox.ShowAsync();
+
+            }
+        }
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
         /// <summary>
         /// Invoked when this page is about to be displayed in a Frame.
         /// </summary>
--- AttendanceMugd/AttendanceMugd.WindowsPhone/obj/Release/ideaPage.g.i.cs
+++ AttendanceMugd/AttendanceMugd.WindowsPhone/obj/Release/ideaPage.g.i.cs
@@ -15,6 +15,22 @@ namespace AttendanceMugd
     partial class ideaPage : global::Windows.UI.Xaml.Controls.Page
     {
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
+        private global::Windows.UI.Xaml.Controls.Image ImageBox; 
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
+        private global::Windows.UI.Xaml.Controls.ProgressBar myProgressBar; 
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
+        private global::Windows.UI.Xaml.Controls.TextBox title; 
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
+        private global::Windows.UI.Xaml.Controls.TextBox option_1; 
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
+        private global::Windows.UI.Xaml.Controls.TextBox option_2; 
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
+        private global::Windows.UI.Xaml.Controls.TextBox option_3; 
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
+        private global::Windows.UI.Xaml.Controls.Button submit; 
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
+        private global::Windows.UI.Xaml.Controls.Button back; 
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
         private bool _contentLoaded;
 
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 4.0.0.0")]
@@ -27,6 +43,14 @@ namespace AttendanceMugd
             _contentLoaded = true;
             global::Windows.UI.Xaml.Application.LoadComponent(this, new global::System.Uri("ms-appx:///ideaPage.xaml"), global::Windows.UI.Xaml.Controls.Primitives.ComponentResourceLocation.Application);
  
+            ImageBox = (global::Windows.UI.Xaml.Controls.Image)this.FindName("ImageBox");
+            myProgressBar = (global::Windows.UI.Xaml.Controls.ProgressBar)this.FindName("myProgressBar");
+            title = (global::Windows.UI.Xaml.Controls.TextBox)this.FindName("title");
+            option_1 = (global::Windows.UI.Xaml.Controls.TextBox)this.FindName("option_1");
+            option_2 = (global::Windows.UI.Xaml.Controls.TextBox)this.FindName("option_2");
+            option_3 = (global::Windows.UI.Xaml.Controls.TextBox)this.FindName("option_3");
+            submit = (global::Windows.UI.Xaml.Controls.Button)this.FindName("submit");
+            back = (global::Windows.UI.Xaml.Controls.Button)this.FindName("back");
         }
     }
 }
